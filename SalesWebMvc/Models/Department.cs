0879fdbb71ec81e0ffdaa8511d6de6b587d3d161
JeoftonCosta <jeofton_costa@hotmail.com>
2021-04-03 12:28:01 -0300

    Implementando outras entidades

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,29 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        //Referenciando as associações entre entidades
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        //Criando os construtores
+        //Construtor vazio (mandatório, pois serão criados construtores com argumentos)
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
